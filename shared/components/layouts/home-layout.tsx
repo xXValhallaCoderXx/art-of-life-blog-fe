a57eeccfba8eeca283c3a1e9086105cf5283fecf
xXValhallaCoderXx <renate.gouveia@gmail.com>
2020-01-21 18:52:23 +0800

    Add meta

--- shared/components/layouts/home-layout.tsx
+++ shared/components/layouts/home-layout.tsx
@@ -1,21 +1,48 @@
+import Head from "next/head";
 import { makeStyles } from "@material-ui/core/styles";
 import { TopNav } from "../navigation";
 import Container from "@material-ui/core/Container";
+import MY_SEO from "shared/utils/seo-meta";
+
+interface Props {
+  title?: string;
+  description?: string;
+  image?: string;
+  children: React.ReactNode;
+}
 
 const useStyles = makeStyles(theme => ({
   innerContainer: {
-    paddingTop: 100,
+    paddingTop: 100
   }
 }));
 
-export default ({ children }: any) => {
+export default ({ children, title, description, image }: Props) => {
   // @ts-ignore
   const classes = useStyles();
   return (
     <div>
+      <Head>
+        <title key="title">{MY_SEO.title}</title>
+
+        <meta
+          key="description"
+          name="description"
+          content={MY_SEO.description}
+        />
+        <meta key="og:type" name="og:type" content={MY_SEO.openGraph.type} />
+        <meta key="og:title" name="og:title" content={MY_SEO.openGraph.title} />
+        <meta
+          key="og:description"
+          name="og:description"
+          content={MY_SEO.openGraph.description}
+        />
+        <meta key="og:url" name="og:url" content={MY_SEO.openGraph.url} />
+        <meta key="og:image" name="og:image" content={MY_SEO.openGraph.image} />
+      </Head>
       <div className={classes.innerContainer}>
-      <TopNav />
-      <Container maxWidth="xl">{children}</Container>
+        <TopNav />
+        <Container maxWidth="xl">{children}</Container>
       </div>
     </div>
   );
---
+++ shared/utils/seo-meta.ts
@@ -0,0 +1,13 @@
+const MY_SEO = {
+  title: 'Art Of Life',
+  description: 'This blog is about my travels, hobbies and random thoughts I have about life',
+  openGraph: {
+      type: 'website',
+      url: 'https://www.art-of-life.blog',
+      title: 'Art Of Life',
+      description: 'This blog is about my travels, hobbies and random thoughts I have about life',
+      image: '',
+  }
+};
+
+export default MY_SEO;
