d81c07c2ae2eb575b86f7d080aea27f738a04bcd
xXValhallaCoderXx <renate.gouveia@gmail.com>
2020-01-22 23:11:11 +0800

    Fix meta data

--- shared/utils/seo-meta.ts
+++ shared/utils/seo-meta.ts
@@ -1,12 +1,12 @@
 const MY_SEO = {
   title: 'Art Of Life',
-  description: 'This blog is about my travels, hobbies and random thoughts I have about life',
+  description: 'This blog will be an outlet to post about the things I love in life, such as travel, fitness, technology and a way safe way to express some of the strange thoughts I have about life',
   openGraph: {
       type: 'website',
       url: 'https://www.art-of-life.blog',
       title: 'Art Of Life',
-      description: 'This blog is about my travels, hobbies and random thoughts I have about life',
-      image: '',
+      description: 'This blog will be an outlet to post about the things I love in life, such as travel, fitness, technology and a way safe way to express some of the strange thoughts I have about life',
+      image: 'https://strapi-art-of-life.s3.ap-southeast-1.amazonaws.com/57e26be45a204c44a8c40358d96dbeb1.jpg',
   }
 };
 
