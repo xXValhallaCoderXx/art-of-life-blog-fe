5cb55af9b4c192fbb268762b298f1be18b145983
Renate Gouveia <renate.gouveia@gmail.com>
2020-06-15 02:54:26 +0800

    REmove env config from webpack

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ module.exports = compose([
   [withImages, { inlineImageLimit: 100 }],
   {
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      const env = Object.keys(process.env).reduce((acc, curr) => {
-        acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-        return acc;
-      }, {});
+      // const env = Object.keys(process.env).reduce((acc, curr) => {
+      //   acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      //   return acc;
+      // }, {});
       config.plugins.push(new webpack.DefinePlugin(env));
       // Note: we provide webpack above so you should not `require` it
       // Perform customizations to webpack config
