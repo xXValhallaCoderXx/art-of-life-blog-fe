e72305bbd7d14d461c2f7e1b233644570b231962
xXValhallaCoderXx <renate.gouveia@gmail.com>
2021-05-03 14:50:58 +0800

    Update material uio

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
+import createEmotionServer from "@emotion/server/create-instance";
 import theme from "shared/styles/theme";
+import { cache } from "./_app";
+
+const { extractCritical } = createEmotionServer(cache);
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
@@ -24,6 +28,8 @@ export default class MyDocument extends Document {
   }
 }
 
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
   // Resolution order
   //
@@ -57,6 +63,7 @@ MyDocument.getInitialProps = async (ctx) => {
     });
 
   const initialProps = await Document.getInitialProps(ctx);
+  const styles = extractCritical(initialProps.html);
 
   return {
     ...initialProps,
@@ -64,6 +71,12 @@ MyDocument.getInitialProps = async (ctx) => {
     styles: [
       ...React.Children.toArray(initialProps.styles),
       sheets.getStyleElement(),
+      <style
+        key="emotion-style-tag"
+        data-emotion={`css ${styles.ids.join(" ")}`}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: styles.css }}
+      />,
     ],
   };
 };
